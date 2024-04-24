f6a76439f1e110ca51eb7ba2ad5f25d9ed00e986
kathia230212 <202020083@tese.edu.mx>
2024-04-23 22:46:41 -0600

    actualizacion de ionic

---
+++ src/app/home/r-actualizaciones/r-actualizaciones.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-r-actualizaciones',
+  templateUrl: './r-actualizaciones.component.html',
+  styleUrls: ['./r-actualizaciones.component.scss'],
+})
+export class r-actualizacionesComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
